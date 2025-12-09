3a1f09520c8468defa29a2971986db19805859fc
kirito shikahara <KDT24GPR30001@edu.kobe-tech.ac.jp>
2025-12-10 00:48:22 +0900

    Inventory::InventoryManagerの定義の作成

---
+++ program/Application/Inventory/Inventory/InventoryManager.cpp
@@ -0,0 +1,9 @@
+#include "InventoryManager.hpp"
+
+std::vector<std::string> Inventory::InventoryManager::GetAllNames()
+{
+    // 保存しているコレクションから名前だけを抜き出して
+    // 新しい名前だけのコレクションを作成する
+
+    return std::vector<std::string>();
+}
