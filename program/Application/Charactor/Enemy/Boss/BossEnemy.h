ffa4b34bd8f82ccfe36918a251fbb1f13b61d7f2
yamada <KDT24GPR30005@edu.kobe-tech.ac.jp>
2025-12-14 23:09:40 +0900

    ファイルの作成

---
+++ program/Application/Charactor/Enemy/Boss/BossEnemy.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Application/Charactor/base/CharactorBase.hpp"
+
+class BossEnemy : public Chara::CharaBase
+{
+
+
+};
+
---
+++ program/Application/Charactor/Enemy/Enemy.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Application/Charactor/base/CharactorBase.hpp"
+
+namespace Chara
+{
+	/// <summary>
+	///	エネミークラス
+	/// </summary>
+	class Enemy : public Chara::CharaBase
+	{
+
+	};
+}
+
