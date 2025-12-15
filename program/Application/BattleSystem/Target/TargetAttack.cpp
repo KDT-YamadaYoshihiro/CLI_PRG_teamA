04ee874c2056481f10333cc94f927a371910eb19
Yamada Yoshihiro <kdt24gpr30005@edu.kobe-tech.ac.jp>
2025-12-15 12:03:51 +0900

    型変換をto_stringを使用しての変換に修正

--- program/Application/BattleSystem/Target/TargetAttack.cpp
+++ program/Application/BattleSystem/Target/TargetAttack.cpp
@@ -6,20 +6,18 @@
 
 void Battle::Action::Attack(Chara::Player* player, std::vector<Chara::Enemy> enemyList)
 {
-
-	std::string count = { "%d",enemyList.size() };
+	std::string count = std::to_string(enemyList.size());
 	// 対象の選択案内
 	CLI_ENGINE->GetView()->AddLine("対象を０から" + count + "の中から選んでください");
 
 	// cinを使用して入力
-	int number;
-	std::cin >> number;
+	int number = enemyList.size();
 	// 選択範囲内になるまでループ
-	while (number <= enemyList.size())
+	while (number >= enemyList.size())
 	{
+		std::cin >> number;
 		// 対象の選択案内
 		CLI_ENGINE->GetView()->AddLine("選択範囲外です。もう一度入力してください");
-		std::cin >> number;
 	}
 
     // ダメージ計算
@@ -27,7 +25,7 @@ void Battle::Action::Attack(Chara::Player* player, std::vector<Chara::Enemy> ene
 	// 計算値を対象に与える。
 	enemyList[number].ApplyDamage(damage);
 	// 表示用ダメージ値を作成
-	std::string log_damage = { "%d",damage };
+	std::string log_damage = std::to_string(damage);
 	// ログを出す
 	CLI_ENGINE->GetView()->AddLine(player->GetName() + "は" + enemyList[number].GetName() + "に" + log_damage + "を与えた。");
 
